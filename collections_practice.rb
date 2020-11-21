13ce46cba679c48e5dc535dcbafbcfb5bb507ea4
Nsingletary <singletarybush@gmail.com>
2020-11-21 15:26:59 -0500

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,18 @@
-
+def sort_array_asc(array)
+    array.sort
+  end
+  
+  def sort_array_desc(array)
+    array.sort.reverse
+  end
+  
+  def sort_array_char_count(array)
+    array.sort do |a, b|
+      if a.length < b.length
+        -1
+      elsif a.length == b.length
+        0
+      elsif a.length > b.length
+        
+      end
+    end  
